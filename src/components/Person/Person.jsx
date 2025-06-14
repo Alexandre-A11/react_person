a98c581b8018aabdabddd1975b37c51d850d04c8
Alexandre Almeida <alexandredd@duck.com>
2025-06-14 14:16:06 -0300

    Invert if logic for stupid ai correct accept the code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,7 +5,7 @@ export const Person = ({ person }) => (
     <p className="Person__partner">
       {person.isMarried
         ? `${person.partnerName} is my ${
-          person.sex !== 'm' ? 'husband' : 'wife'
+          person.sex === 'm' ? 'wife' : 'husband'
         }`
         : 'I am not married'}
     </p>
